b56628c82b42f100a9517de784dd01314ee50f35
campers98 <140605897+campers98@users.noreply.github.com>
2024-05-26 17:11:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "YOUR_BOT_TOKEN"
-MONGO_DB_URL = "YOUR_MONGO_DB_URL"
-AI_API_KEY = "RBPOWF2m8z85prBQ"
-AI_BID = "171092"
+TOKEN = "6369515689:AAFFkkFGtxmbA19zHSEAS7pEeOzsluL29Yg"
+MONGO_DB_URL = "mongodb://clve73lfu001ha4me1al6gw90:UTfT7DjJGjwyvbdXaM1hDrVj@104.251.218.202:9013/?readPreference=primary&ssl=false"
+AI_API_KEY = "7Y5hgMnlTjtjCyz0"
+AI_BID = "182191"
