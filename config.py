65be1885ace872fe1dbf3536c61de15d4836881a
campers98 <140605897+campers98@users.noreply.github.com>
2024-05-27 11:36:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from os import getenv
 TOKEN = "6369515689:AAFFkkFGtxmbA19zHSEAS7pEeOzsluL29Yg"
 MONGO_DB_URL = "mongodb://clve73lfu001ha4me1al6gw90:UTfT7DjJGjwyvbdXaM1hDrVj@104.251.218.202:9013/?readPreference=primary&ssl=false"
 AI_API_KEY = "7Y5hgMnlTjtjCyz0"
-AI_BID = "182191"
-BOT_USERNAME = "Minisvd_bot"
+AI_BID = "182191"s
 LOAD = getenv("LOAD", "").split()
 NO_LOAD = getenv("NO_LOAD", "False").split()
