cbc0dafb35ad8a1f1bedb3b157493ac837336050
campers98 <140605897+campers98@users.noreply.github.com>
2024-05-27 10:05:17 +0530

    Create start.py

---
+++ botsatu/modules/start.py
@@ -0,0 +1,52 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CommandHandler, ContextTypes
+
+from botsatu import rani
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat = update.effective_chat
+    msg = update.effective_message
+    akboss = []
+    akboss.append(
+        [
+            InlineKeyboardButton(
+                text="𝐀ᴅᴅ 𝐌ᴇ",
+                url=f"http://t.me/{context.bot.username}?startgroup=true"),
+            InlineKeyboardButton(
+                text="𝐃ᴀʀʟɪɴɢ",
+                url=f"https://t.me/Idhayann"),
+        ]
+    
+    )
+    await msg.reply_text(
+        f"ʜᴇʏᴀ\nɪ'ᴍ {context.bot.first_name}\nɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴀᴄᴛɪᴠᴇ ʏᴏᴜʀ ᴄʜᴀᴛ \n",
+        reply_markup=InlineKeyboardMarkup(akboss),
+    )
+
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat = update.effective_chat
+    msg = update.effective_message
+    akboss = []
+    akboss.append(
+        [
+            InlineKeyboardButton(
+                text="𝐀ᴅᴅ 𝐌ᴇ",
+                url=f"http://t.me/{context.bot.username}?startgroup=true"),
+            InlineKeyboardButton(
+                text="𝐃ᴀʀʟɪɴɢ",
+                url=f"https://t.me/Idhayann"),
+        ]
+    )
+    await msg.reply_text(
+        f"ʜᴇʏᴀ\nɪ'ᴍ {context.bot.first_name}\nɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴀᴄᴛɪᴠᴇ ʏᴏᴜʀ ᴄʜᴀᴛ \n\nɪ ᴄᴀɴ ʀᴇꜱᴛʀɪᴄᴛ ᴡʜɪᴄʜ ᴄᴏɴᴛᴀɪɴꜱ ᴘᴜʙʟɪᴄ ᴄʜᴀᴛ ᴜꜱᴇʀɴᴀᴍᴇ ᴍᴇꜱꜱᴀɢᴇꜱ",
+        reply_markup=InlineKeyboardMarkup(akboss),
+    )
+
+
+START = CommandHandler(["chatbot", "start"], start, block=False)
+HELP = CommandHandler(["chelp", "help"], help, block=False)
+
+rani.add_handler(START)
+rani.add_handler(HELP)
